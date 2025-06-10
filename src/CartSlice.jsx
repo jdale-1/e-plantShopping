f67a0f810d47bbfcf0a9eb58519906969fa404ca
jr <lloyd.dale.jr@gmail.com>
2025-06-10 17:51:11 -0400

    initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -17,6 +17,7 @@ export const CartSlice = createSlice({
     // If item does not exist, add it to the cart with quantity 1
     state.items.push({ name, image, cost, quantity: 1 });
   }
+  
 },
     removeItem: (state, action) => {
         state.items = state.items.filter(item => item.name !== action.payload);
