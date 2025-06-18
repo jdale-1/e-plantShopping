0c489aa4c82dfed50df8d77a0b638f1de08b61db
jr <lloyd.dale.jr@gmail.com>
2025-06-18 16:07:16 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -8,6 +8,14 @@ function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+      
+        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+          ...prevState, // Spread the previous state to retain existing entries
+          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+        }));
+      };
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -300,14 +308,6 @@ function ProductList({ onHomeClick }) {
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
           >
             Add to Cart
-            const handleAddToCart = (product) => {
-        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
-        }));
-      };
           </button>
         </div>
       ))}
