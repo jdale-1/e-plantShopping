2bcd34a664d902df967a9216069cb34aaf99ca73
jr <lloyd.dale.jr@gmail.com>
2025-06-18 17:58:28 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -7,15 +7,7 @@ import addItem from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
-    const handleAddToCart = (product) => {
-        dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
-      
-        setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
-          ...prevState, // Spread the previous state to retain existing entries
-          [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
-        }));
-      };
+    
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -302,7 +294,7 @@ function ProductList({ onHomeClick }) {
           <div className="product-title">{plant.name}</div> {/* Display plant name */}
           {/* Display other plant details like description and cost */}
           <div className="product-description">{plant.description}</div> {/* Display plant description */}
-          <div className="product-cost">${plant.cost}</div> {/* Display plant cost */}
+          <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
           <button
             className="product-button"
             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
