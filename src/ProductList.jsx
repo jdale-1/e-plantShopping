b29beaa459e49da7ad4f0b67ae37cac49791c6f0
jr <lloyd.dale.jr@gmail.com>
2025-10-28 17:56:04 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem'
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
